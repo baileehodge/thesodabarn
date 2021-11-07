8ba28fc83e7b238ad460116939015f749eaed68c
Justin McBride <jstnmcbrd@outlook.com>
2021-11-06 20:39:19 -0600

    Pages

--- thesodabarn/src/router/index.js
+++ thesodabarn/src/router/index.js
@@ -1,29 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Menu from '../views/Menu.vue'
+import Locations from '../views/Locations.vue'
+import Bakery from '../views/Bakery.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/menu',
+		name: 'Menu',
+		component: Menu
+	},
+	{
+		path: '/locations',
+		name: 'Locations',
+		component: Locations
+	},
+	{
+		path: '/bakery',
+		name: 'Bakery',
+		component: Bakery
+	}
+	//{
+	//	path: '/about',
+	//	name: 'About',
+	//	// route level code-splitting
+	//	// this generates a separate chunk (about.[hash].js) for this route
+	//	// which is lazy-loaded when the route is visited.
+	//	component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	//}
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 })
 
 export default router
