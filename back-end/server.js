7730a505f9d45e1cdd915fcba2f3dde639fe3de4
Bailee Hodge <baileebyu@gmail.com>
2021-12-04 16:50:02 -0700

    mongo stuff

--- back-end/server.js
+++ back-end/server.js
@@ -10,17 +10,37 @@ app.use(bodyParser.urlencoded({
 const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
+mongoose.connect('mongodb://localhost:27017/thesodabarn', {
   useNewUrlParser: true
 });
 
-// Get a list of all of the items in the museum.
-let mock = require('./cookie-data.json').cookies;
+// Create a scheme for cookies: a title, a price, and a path to an image.
+const cookieSchema = new mongoose.Schema({
+  name: String,
+  price: String,
+  image: String,
+});
+// Create a model for the cookies
+const Cookie = mongoose.model('Cookie', cookieSchema);
+
+//preload cookie database if it's empty
+Cookie.find().then(cookies => {
+  if (cookies.length != 0) return;
+  let mock = require('./cookie-data.json').cookies;
+  for (c of mock) {
+    const cookie = new Cookie({
+      name: c.name,
+      price: c.price,
+      image: c.image
+    });
+    cookie.save();
+  }
+});
+
 app.get('/api/cookies', async (req, res) => {
   try {
-    //let items = await Item.find();
-    //res.send(items);
-    res.send(mock);
+    let cookies = await Cookie.find();
+    res.send(cookies);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
